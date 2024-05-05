70c89ae957470a93a1c11e29f0f6212a5f4856c2
Karkaratz <pixelface@techie.com>
2024-05-05 16:53:01 +0200

    Firs Commit with example on chromadb http client.

--- src/vector-db/faiss/insert-faiss.py
+++ src/vector-db/faiss/insert-faiss.py
@@ -1,20 +1,17 @@
-import hashlib
-
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-import chromadb
+from langchain_community.vectorstores import FAISS
 
 
 load_dotenv()
 # Path of the document to read (This example explores the loading of a single document)
 pdf_path = "<YOUR_FILE_PATH_HERE>"
-# The collection to use in chromadb
-collection_name = "<YOUR_COLLECTION_HERE>"
-# Information for chromadb http requests
-chroma_host = "<YOUR_HOST_ADDRESS_HERE>"
-chroma_port = 8000  # Change with your port
+
+# Full path and file_name for your FAISS index.
+save_path = "<YOUR_FILE_PATH_HERE/YOUR_FILENAME"
+
 # How to split and embed the text
 chunk_size = 300    # change with your size
 chunk_overlap = 10  # change with your overlap size.
@@ -23,20 +20,14 @@ chunk_overlap = 10  # change with your overlap size.
 # In this example, I'm using LM Studio with nomic-ai/nomic-embed-text-v1.5-GGUF
 # If you want to run it agains standard OpenAI embeddings, use the standard OpenAI configuration.
 embeddings = OpenAIEmbeddings(
-    base_url="http://localhost:1234/v1", # My LM studio is running on localhost, port 1234
-    api_key="not-needed", # My LM Studio does not require a password.
-    model="gpt2", # Just a string. My LM Studio does not require to specify the model. Needed in other cases.
+    base_url="http://localhost:1234/v1",    # My LM studio is running on localhost, port 1234.
+    api_key="not-needed",   # My LM Studio does not require a password.
+    model="gpt2",           # Just a string. My LM Studio does not require to specify the model. Needed in other cases.
     tiktoken_enabled=False
 )
 
 if __name__ == "__main__":
-    # Create a Chroma DB Http Client
-    client = chromadb.HttpClient(host=chroma_host, port=chroma_port)
-    # list all collections
-    print(client.list_collections())
-    # Using the client, get an instance of a specific collection.
-    # If the collection does not exists, it creates a new collection.
-    collection = client.get_or_create_collection(collection_name)
+
     print("Job Started")
 
     # Instantiation a Loader specialized on PDF files.
@@ -64,22 +55,5 @@ if __name__ == "__main__":
     docs = text_splitter.split_documents(documents=documents)
     print(len(docs)) # a simple print of how many chunks we have generate
 
-    # Preparing for writing into vector db. Chroma in our example.
-    # Just remember: an item written into a vector database is calle document.
-    # Creating 4 lists
-    #   1 page_contents - The original content of chunks
-    #   2 ids - unique identifier of our documents
-    #   3 metadata - metadata gathered from the original pdf file
-    #   4 x - embedding results
-    page_contents = [doc.page_content for doc in docs if doc.page_content]
-    ids = [hashlib.sha256(doc.page_content.encode("utf-8")).hexdigest() for doc in docs if doc.page_content]
-    metadata = [doc.metadata for doc in docs if doc.page_content]
-    x = embeddings.embed_documents(page_contents, 300)
-    # write everything into the vector db, using the collection object gathered from chroma client.
-    collection.upsert(
-        documents=page_contents,
-        embeddings=x,
-        metadatas=metadata,
-        ids=ids,
-    )
-    print("Completed")
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local(save_path)
